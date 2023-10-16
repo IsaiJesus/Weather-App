ab547ffc03716c3694c3e12ef8dbb344148c1db4
Isai Jesus <isaijesus02@gmail.com>
2023-10-16 17:30:32 -0600

    Changing size details

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -6,7 +6,7 @@ export default function Day({icon, temperature, weekday}) {
   const iconImg = imageAssociation(icon);
 
   return (
-    <Col className="bg-primary-subtle py-1 m-1 text-center rounded">
+    <Col className=" bg-primary-subtle py-1 m-1 text-center rounded" style={{minWidth: "160px"}}>
       <img className="p-1" style={{width: "80px"}} src={iconImg} alt="Weather Icon" />
       <h3 className="fw-bold fs-2">{temperature} °C</h3>
       <p className="mb-1 p-1">{weekday}</p>
